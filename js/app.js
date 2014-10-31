250bb8839f4688756d6e7ab7720af45459633873
Alex Schenck <AlexNSchenck@gmail.com>
2014-10-31 15:04:52 -0700

    Finished formatting

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 function onReady () {
 	var ul = document.getElementById("tiles");
+	var gameInfo = document.getElementById("game-info");
 
 	for (var i = 0; i < 16; i++) {
 		var newLI = document.createElement("li");
@@ -11,6 +12,10 @@ function onReady () {
 		newLI.appendChild(newImg);
 		ul.appendChild(newLI);
 	}
+}
+
+document.getElementById("new-game").onclick = function() {
+	window.location.reload();
 }
 
 $(onReady);
