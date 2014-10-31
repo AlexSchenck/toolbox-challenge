805abcc0d5a466caa07cf7d2032919a875fafc0f
Alex Schenck <AlexNSchenck@gmail.com>
2014-10-31 14:15:57 -0700

    Images added dynamically

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 "use strict";
+
+function onReady () {
+	var ul = document.getElementById("tiles");
+
+	for (var i = 0; i < 16; i++) {
+		var newLI = document.createElement("li");
+		var newImg = document.createElement("img");
+		newImg.src = "img/tile-back.png";
+
+		newLI.appendChild(newImg);
+		ul.appendChild(newLI);
+	}
+}
+
+$(onReady);
